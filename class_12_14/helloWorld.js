540377cd57a1e86053d2561c75ca15ad9af2b583
BigMan <IIAchilles@protonmail.com>
2025-07-07 17:55:03 +0300

    javascript.info | 2.5 data types done

--- class_12_14/helloWorld.js
+++ class_12_14/helloWorld.js
@@ -1,7 +1,14 @@
+// "use strict";
+
 // Show an alert
 
 //console.log("hello world");
 
 // Show an alert with an external script 
 
-alert("Hello World from exteranl file")
+// alert("Hello World from exteranl file")
+
+// This work without use strict.
+// num = 5;
+
+// alert(num);
