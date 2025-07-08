037107611c16daaa4c4443c6b5b3afe8b6983434
BigMan <IIAchilles@protonmail.com>
2025-07-08 18:30:53 +0300

    created javascript.info folder

---
+++ javascript.info/2_8_basicOperatorsMaths.js
@@ -0,0 +1,73 @@
+// Tasks
+
+// The postfix and prefix forms
+
+// let a = 1, b = 1;
+
+// let c = ++a; // returns 2
+// let d = b++ // returns 1
+
+//What are the final values of all variables a, b, c and d after the code below?
+
+// a = 2;
+// b = 2;
+// c = 2;
+// d = 1;
+
+// copy into browser: 
+
+// let a = 1, b = 1;
+// let c = ++a; // returns 2
+// let d = b++ // returns 1
+// alert(`a is ${a}`); // 2
+// alert(`b is ${b}`); // 2
+// alert(`c is ${c}`); // 2
+// alert(`d is ${d}`); // 1
+
+
+// Assignment result
+//What are the values of a and x after the code below?
+
+// let a = 2;
+
+// let x = 1 + (a *= 2);
+// alert(a);
+// alert(x);
+//a = 4
+//x = 5
+
+//Type conversions
+// "" + 1 + 0 //10 correct
+// "" - 1 + 0 //-10
+// true + false // 1
+// 6 / "3" //2
+// "2" * "3" //6
+// 4 + 5 + "px" //9px
+// "$" + 4 + 5 // $45
+// "4" - 2 // 2
+// "4px" - 2 // NaN
+// "  -9  " + 5 //   -9  5
+// "  -9  " - 5 // -14
+// null + 1 // 1
+// undefined + 1 // NaN
+// " \t \n" - 2 // error
+
+/////
+
+// "" + 1 + 0 = "10" // (1)
+// "" - 1 + 0 = -1 // (2)
+// true + false = 1
+// 6 / "3" = 2
+// "2" * "3" = 6
+// 4 + 5 + "px" = "9px"
+// "$" + 4 + 5 = "$45"
+// "4" - 2 = 2
+// "4px" - 2 = NaN
+// "  -9  " + 5 = "  -9  5" // (3)
+// "  -9  " - 5 = -14 // (4)
+// null + 1 = 1 // (5)
+// undefined + 1 = NaN // (6)
+// " \t \n" - 2 = -2 // (7)
+
+// Fix the addition
+
